c1f9b1d1d9826bf6a15be075d4f7fefd9174d73e
Wuhlulu <lsypixel464@outlook.com>
2025-10-25 01:45:40 +0800

    feat; 完成作业

--- hw06b/hw06b_02.m
+++ hw06b/hw06b_02.m
@@ -1,24 +1,23 @@
+clf
+
 h = 0.001;
 x = 0:h:8;
 y = -1./6.*x.^3 + 1./120.*x.^5 - 1./5040.*x.^7 + 1./ 362880.*x.^9;
-fun = @(x) -1./6.*x.^3 + 1./120.*x.^5 - 1./5040.*x.^7 + 1./ 362880.*x.^9;
+fun = @(x) y(8001) - (-1./6.*x.^3 + 1./120.*x.^5 - 1./5040.*x.^7 + 1./ 362880.*x.^9);
+c = y(8001)*ones(1,length(x));
+H = c-y;
 format long
 
 disp("左矩形")
-z1 = sum(y(1:length(x)-1))
+z1 = sum(H(1:length(x)-1))*h
 
 disp("右矩形")
-z2 = sum(y(2:length(x)))
+z2 = sum(H(2:length(x)))*h
 
 disp("梯形")
-z3 = trapz(x,y)
+z3 = trapz(x,H)
 
 disp("自适应辛普森")
 z4 = integral(fun,0,8)
 
-plot([x,0,0],[y,0,0],"b")
-
-X = [0 5; 0 5; 4 9];
-Y = [2 0; 4 2; 2 0];
-C = [0 1];
-fill(X,Y,C)
+fill([x, 0], [y, y(8001)], "b")
